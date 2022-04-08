f4006846c0b35b554b17568be8b71eb1da72b19d
Yaroslav Bondarenko <48105643+dakota1273@users.noreply.github.com>
2022-04-08 13:28:44 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,17 +1,12 @@
 package com.company;
-import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String name;
-        System.out.println("Введите имя: ");
-        name = scanner.next();
-        if (name.contains("Вячеслав")) {
-            System.out.print("Привет, " + name);
-        } else {
-            System.out.print("Нет такого имени");
+    for (int i = 1; i <= 150; i++){
+        if (i%3==0) {
+            System.out.println(i);
         }
+    }
     }
 }
